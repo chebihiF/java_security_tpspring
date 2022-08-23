541ee91b45005afa518389ff105b9cc49529cc73
chebihiF <chebihi.f@ucd.ac.ma>
2022-08-23 08:42:28 +0100

    add hello.jsp

--- src/main/java/org/cda/tpspring/MyServlet.java
+++ src/main/java/org/cda/tpspring/MyServlet.java
@@ -1,16 +1,26 @@
 package org.cda.tpspring;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet("/hello")
 public class MyServlet extends HttpServlet {
 
     @Override //IOC ==> tomcat (req, resp)
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html");
-        resp.getWriter().println("Hello from <b>Servlet</b>");
+        // trait
+        String name = req.getParameter("name");
+        String IP = req.getRemoteAddr().toString();
+
+        req.setAttribute("name",name);
+        req.getRequestDispatcher("hello.jsp").forward(req,resp);
+
+        /*resp.setContentType("text/html");
+        resp.getWriter().println("Hello from <b>Servlet</b> by "+name);
+        resp.getWriter().println("IP : "+IP);*/
     }
 }
---
+++ src/main/webapp/hello.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: fcheb
+  Date: 8/23/2022
+  Time: 8:27 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    Hello from <b>MyServlet</b> <br>
+    Name : ${name}
+</body>
+</html>
